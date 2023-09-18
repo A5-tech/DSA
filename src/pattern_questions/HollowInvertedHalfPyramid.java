9fd41e9fd43a36167b6188dc1ae0c086175918b0
A5-tech <anas.jamiahamdard.btech.cs@gmail.com>
2023-09-18 15:20:10 +0530

    Make pattern row independent

--- src/pattern_questions/HollowInvertedHalfPyramid.java
+++ src/pattern_questions/HollowInvertedHalfPyramid.java
@@ -2,16 +2,17 @@ package pattern_questions;
 
 public class HollowInvertedHalfPyramid {
     public static void main(String[] args) {
-        for(int r=1;r<7;r++){
-            if(r==1 || r==5 || r==6){
-                for(int i=1;i<7-r+1;i++){
+        int n=10;
+        for(int r=1;r<n;r++){
+            if(r==1 || r==n-2 || r==n-1){
+                for(int i=1;i<n-r+1;i++){
                     System.out.print(" * ");
                 }
                 System.out.println();
             }
            else {
-                for(int c=1;c<7-r+1;c++){
-                    if(c==1 || c==7-r){
+                for(int c=1;c<n-r+1;c++){
+                    if(c==1 || c==n-r){
                         System.out.print(" * ");
                     }
                     else {
--- src/pattern_questions/HollowRectangle.java
+++ src/pattern_questions/HollowRectangle.java
@@ -2,9 +2,10 @@ package pattern_questions;
 
 public class HollowRectangle {
     public static void main(String[] args) {
-        for(int r=1;r<4;r++){
-            for(int c=1;c<6;c++){
-                if(c!=1 && c!=5 && r==2){
+        int rows=5,columns=8;
+        for(int r=1;r<=rows;r++){
+            for(int c=1;c<=columns;c++){
+                if(c!=1 && c!=columns && r!=1 && r!=rows){
                     System.out.print("   ");
                 }
                 else {
